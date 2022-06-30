1164c83804c45e9ad4df4371e9411882c038b0a8
jehadalhorany <alhoranyjehad@gmail.com>
2022-06-30 15:09:46 +0300

    add compoonant CV

--- my-app/src/componant/CV/index.js
+++ my-app/src/componant/CV/index.js
@@ -7,11 +7,12 @@ const Cv = () => {
 
     return (<div>
 
-        <p className="cv_p">I'm Jehad ALhorane  <span> Full stack developer</span></p>
-        <img src="https://res.cloudinary.com/jehadforcars/image/upload/v1656526417/0db44162773ca93b3870d5047fd8e925adafed3e-fococlipping-standard_g1xxbp.png" className="img_cv"/>
-        <br/>
+        <p className="cv_p"><b>I'm  Jehad ALhorane</b>   <span><b>Full stack developer</b></span></p>
+        <p className="pro_p"><b>Positive person, learnable, I love teamwork,<br/> my ambitionis to be among the largest developer team in the world</b></p>
+        <img src="https://res.cloudinary.com/jehadforcars/image/upload/v1656526417/0db44162773ca93b3870d5047fd8e925adafed3e-fococlipping-standard_g1xxbp.png" className="img_cv" />
+        <br />
         <a href="https://1drv.ms/b/s!Av-gxGidmS0CjKR8wQWnx1nP5aSJLw?e=VNWMfC" className='links'>link resume</a>
-       
+
     </div>
     )
 }
