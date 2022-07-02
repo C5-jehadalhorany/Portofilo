4bfe5cb4408eaa7ef5683ef1a6a59f03dce8c25d
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-02 12:56:05 +0300

    add the links and title

--- my-app/src/componant/CV/index.js
+++ my-app/src/componant/CV/index.js
@@ -5,14 +5,16 @@ import("./style.css")
 
 const Cv = () => {
 
-    return (<div>
-
+    return (<div className="cv_div">
+        <div className="P_div_cv">
         <p className="cv_p"><b>I'm  Jehad ALhorane</b>   <span><b>Full stack developer</b></span></p>
-        <p className="pro_p"><b>Positive person, learnable, I love teamwork,<br/> my ambitionis to be among the largest developer team in the world</b></p>
+        <p className="pro_p">Positive person, learnable, I love teamwork,my ambitionis <br/>to be among the largest developer team in the world</p>
+        </div>
+        <div className="img_andlink_cv">
         <img src="https://res.cloudinary.com/jehadforcars/image/upload/v1656526417/0db44162773ca93b3870d5047fd8e925adafed3e-fococlipping-standard_g1xxbp.png" className="img_cv" />
         <br />
         <a href="https://1drv.ms/b/s!Av-gxGidmS0CjKR8wQWnx1nP5aSJLw?e=VNWMfC" className='links'>link resume</a>
-
+        </div>
     </div>
     )
 }
