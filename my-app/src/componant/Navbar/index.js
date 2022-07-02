ac609632303cccd5f96e4f0d7213acf056ba5958
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-02 12:58:19 +0300

    add commponant and Eidt

--- my-app/src/componant/Navbar/index.js
+++ my-app/src/componant/Navbar/index.js
@@ -1,22 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Container,Nav} from 'react-bootstrap'
+import { Navbar, Container, Nav, Image } from 'react-bootstrap'
 import("./style.css")
 
 
 
- const NavbaR = () => {
+const NavbaR = () => {
 
-    return <Navbar bg="secondary"  variant="dark"  >
+    return <Navbar bg="secondary" variant="dark"  >
         <Container className='primary'>
-            <Navbar.Brand href="/">Portofilo</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="https://1drv.ms/b/s!Av-gxGidmS0CjKR8wQWnx1nP5aSJLw?e=VNWMfC">Resume</Nav.Link>
-                <Nav.Link href="">MyWork</Nav.Link>
+            <Navbar.Brand href="/" className='home4'>  <img
+                src="https://res.cloudinary.com/jehadforcars/image/upload/v1656621513/Screenshot_6_zljwid.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+            />Portofilo</Navbar.Brand>
+            <Nav className="me-left ">
+                <Nav.Link href="#home" className='home1'>Home</Nav.Link>
+                <Nav.Link href="https://1drv.ms/b/s!Av-gxGidmS0CjKR8wQWnx1nP5aSJLw?e=VNWMfC" className='home2'>Resume</Nav.Link>
+                <Nav.Link href="" className='home3'>MyWork</Nav.Link>
             </Nav>
         </Container>
-    </Navbar>
-    
+    </Navbar >
+
 }
 
 export default NavbaR
