9d0b5b641d93cb425723b564d122f860276500cd
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-02 12:57:21 +0300

    add fnuction and onther

--- my-app/src/componant/Mywork/index.js
+++ my-app/src/componant/Mywork/index.js
@@ -1,44 +1,43 @@
-import {CardGroup,Card} from 'react-bootstrap';
+import {CardGroup,Card,Nav} from 'react-bootstrap';
 import("./style.css")
 
 function GroupExample() {
     return (
         <CardGroup>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+            {/* <Card>
+                <Card.Img variant="top" src="https://res.cloudinary.com/jehadforcars/image/upload/v1656750520/image_5_ovpfqm.png" className='img_mywork' />
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>Portofilo</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.
+                    My achievements, projects and some information about me .{' '}
                     </Card.Text>
+                    <a href="https://ecmashop.netlify.app/" className='ECMA_LINK'>Portofilo </a>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted">to contact me</small>
                 </Card.Footer>
-            </Card>
+            </Card> */}
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="https://res.cloudinary.com/jehadforcars/image/upload/v1656534279/cc_uo1fiy.png" className='img_mywork' />
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>ECMA TEAM</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to
-                        additional content.{' '}
+                    Electrical products international classifications with prices and quantities.{' '}
                     </Card.Text>
+                    <a href="https://ecmashop.netlify.app/" className='ECMA_LINK'>ECMA </a>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">The Project Group Team</small>
                 </Card.Footer>
             </Card>
             <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Img variant="top" src="https://res.cloudinary.com/jehadforcars/image/upload/v1656749812/myapp_oc7j7w.jpg" className='img_mywork'/>
                 <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>CAR FOR RINT</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in
-                        to additional content. This card has even longer content than the
-                        first to show that equal height action.
+                    A car rental site with international brands of cars and can get 2 cars.{' '}
                     </Card.Text>
+                    <a href="https://ecmashop.netlify.app/" className='ECMA_LINK'>CAR FOR RINT</a>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">The Solo Project</small>
