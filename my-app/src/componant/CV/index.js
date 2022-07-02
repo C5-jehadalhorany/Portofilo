a562683dacbe2d6b302f1e6e597c410d5b305149
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-02 14:26:09 +0300

    Eidt for img tag

--- my-app/src/componant/CV/index.js
+++ my-app/src/componant/CV/index.js
@@ -8,7 +8,7 @@ const Cv = () => {
     return (<div className="cv_div">
         <div className="P_div_cv">
         <p className="cv_p"><b>I'm  Jehad ALhorane</b>   <span><b>Full stack developer</b></span></p>
-        <p className="pro_p">Positive person, learnable, I love teamwork,my ambitionis <br/>to be among the largest developer team in the world</p>
+        <p className="pro_p">Positive person, learnable, I love teamwork,my ambitionis  <br/>and to be among the largest developer team in the world</p>
         </div>
         <div className="img_andlink_cv">
         <img src="https://res.cloudinary.com/jehadforcars/image/upload/v1656526417/0db44162773ca93b3870d5047fd8e925adafed3e-fococlipping-standard_g1xxbp.png" className="img_cv" />
