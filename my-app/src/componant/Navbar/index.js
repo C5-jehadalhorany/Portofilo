df9fdfe58b506c24c70ba69970c695cf414ab424
jehadalhorany <alhoranyjehad@gmail.com>
2022-07-03 21:05:57 +0300

    Eidt style and js

--- my-app/src/componant/Navbar/index.js
+++ my-app/src/componant/Navbar/index.js
@@ -7,16 +7,16 @@ import("./style.css")
 const NavbaR = () => {
 
     return <Navbar bg="secondary" variant="dark"  >
-          <img
+
+        <Container className='primary'>
+            <Navbar.Brand href="/" className='home4'> <img
                 src="https://res.cloudinary.com/jehadforcars/image/upload/v1656621513/Screenshot_6_zljwid.png"
                 width="30"
                 height="30"
                 className="d-inline-block align-top"
                 alt="React Bootstrap logo"
-            />
-        <Container className='primary'>
-            <Navbar.Brand href="/" className='home4'>  Portofilo</Navbar.Brand>
-          
+            /> Portofilo</Navbar.Brand>
+
             <Nav className="me-left ">
                 <Nav.Link href="#home" className='home1'>Home</Nav.Link>
                 <Nav.Link href="https://1drv.ms/b/s!Av-gxGidmS0CjKR8wQWnx1nP5aSJLw?e=VNWMfC" className='home2'>Resume</Nav.Link>
